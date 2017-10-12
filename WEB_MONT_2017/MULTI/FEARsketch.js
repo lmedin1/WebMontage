20213cd33a6e5a8f4c62c52620f05bc278bb663e
lmedin1 <lmedin1@saic.edu>
2017-10-11 19:20:51 -0500

    WebMontage
    Obsession compilation

---
+++ WEB_MONT_2017/MULTI/FEARsketch.js
@@ -0,0 +1,49 @@
+var myString = "I wanted to fear you.";
+
+var splitString;
+
+var word = 0;
+
+var xpos = 100;
+var ypos = 200;
+
+var xOff = 0;
+var yOff = 0;
+
+
+function setup() {
+  createCanvas(1300, 650);
+  splitString = split(myString, " ");
+}
+
+function draw(){
+  background(4,79,103);
+
+  fill(99,255,1);
+  textSize(30);
+  //text(splitString[word], 100, 100);
+  //text(splitString[word], xpos, ypos);
+  text(splitString[word], xpos + xOff, ypos + yOff);
+
+  xOff = random(-5, 5);
+  yOff = random(-5, 5);
+}
+
+function mousePressed (){
+  //put fun stuff here
+  //these will do the same thing
+  //word = word +1;
+  //word +=1;
+  word++;
+  //if word is bigger than 6, go back to 0
+  if (word >= splitString.length) {
+    word = 0;
+  }
+
+  xpos = random(0, width - 70);
+  ypos = random(0, height - 40);
+
+  //console.log("My mouse' xpos is" + mouseX)
+  //console.log("My mouse' ypos is" + mouseY)
+
+}
